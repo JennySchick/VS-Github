888d5770314cffee8859ca05c684796c7277be82
AppneticSoftware <accounts@appnetic.de>
2021-12-19 17:08:00 +0100

    First Part Finished

--- Lab/shopping-list/src/App.js
+++ Lab/shopping-list/src/App.js
@@ -1,52 +1,159 @@
 import './App.css';
-import React, { Component } from 'react';
-import { Button } from 'ui-neumorphism';
+import React, { useState, useEffect } from 'react';
+import { Button, TextField, Fab } from 'ui-neumorphism';
 import 'ui-neumorphism/dist/index.css';
-import { SnackbarProvider, useSnackbar } from 'notistack';
-import { Snackbar } from '@mui/material';
+import { useSnackbar } from 'notistack';
+import Item from './item.js';
 
 function App() {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
-  // const Url = process.env.REACT_APP_URL;
-  const handleSend = (e) => {
-    e.preventDefault();
+  var [nameOfNewItem, setNameOfNewItem] = useState('ErrorItem');
+  var items = [
+    { id: '1', name: 'TestItem', quantity: 4, created: 'TK', complete: false },
+    { id: '2', name: 'TestItem', quantity: 3, created: 'TK', complete: false },
+  ];
 
-    // -------------------------------------------
+  useEffect(() => {
     let xhr = new XMLHttpRequest();
-    let rateUrl = 'api/comments';
+    let URL = process.env.REACT_APP_URL + '/items';
+    xhr.open('GET', URL, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send();
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        items = xhr.response;
+        // enqueueSnackbar('Fetched Items from Database.', {
+        //   variant: 'success',
+        // });
+      }
+    };
+  });
+
+  const handleAddItem = (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    let URL = process.env.REACT_APP_URL + 'items';
+    let timeStamp = new Date();
     let object = JSON.stringify({
       id: 1,
-      id2: 2,
+      name: nameOfNewItem,
+      quantity: 1,
+      created: timeStamp.toISOString(),
+      complese: false,
     });
-    xhr.open('POST', rateUrl, true);
+    xhr.open('POST', URL, true);
     xhr.responseType = 'json';
     xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     xhr.send(object);
     xhr.onload = function () {
       if (xhr.status == 200) {
-        if (xhr.response.message == 'valid') {
-          enqueueSnackbar('Danke für die Bewertung', {
-            variant: 'success',
-          });
-        }
+        enqueueSnackbar('Danke für die Bewertung', {
+          variant: 'success',
+        });
       }
     };
+    enqueueSnackbar('Item ' + nameOfNewItem + ' added.', {
+      variant: 'success',
+    });
   };
 
-  const inputFieldsChanged = (e) => {
-    e.preventDefault();
-  };
   return (
     <>
-      <h1>Hello</h1>
       <div
         style={{
-          padding: '1%',
+          display: 'flex',
+          flexDirection: 'column',
+          width: 100 + 'vw',
+          height: 100 + 'vh',
+          maring: 10 + '%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#edf2f4',
         }}
       >
-        <Button onClick={handleSend} />
+        <img
+          style={{
+            width: 150 + 'px',
+            height: 150 + 'px',
+            borderRadius: 75 + 'px',
+            top: 90 + 'px',
+          }}
+          src={require('./cart.png')}
+        />
+        <div
+          style={{
+            display: 'flex',
+            width: 50 + 'vw',
+            height: 60 + 'vh',
+            padding: '1%',
+            margin: 10 + 'px',
+            border: 2 + 'px',
+            borderColor: 'white',
+            borderStyle: 'solid',
+            flexDirection: 'column',
+            borderRadius: 5 + 'px',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <div
+            style={{
+              width: '100%',
+              height: '20%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'row',
+            }}
+          >
+            <TextField
+              label="Add Item Name"
+              className="my-3"
+              onChange={({ target: { value } }) => setNameOfNewItem(value)}
+            ></TextField>
+            <Fab onClick={handleAddItem}>
+              <span style={{ fontSize: '30px' }}>&#43;</span>
+            </Fab>
+          </div>
+          <div
+            style={{
+              width: '100%',
+              height: '80%',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexDirection: 'column',
+            }}
+          >
+            {items.map((item, i) => (
+              <Item item={item}></Item>
+            ))}
+          </div>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            padding: '1%',
+            margin: 10 + 'px',
+            border: 2 + 'px',
+            width: 50 + 'vw',
+            borderColor: 'white',
+            borderStyle: 'solid',
+            borderRadius: 5 + 'px',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <h3
+            style={{
+              paddingRight: 5 + 'px',
+            }}
+          >
+            Amount: {items.length}
+          </h3>
+        </div>
       </div>
-      ;
     </>
   );
 }
---
+++ Lab/shopping-list/src/item.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import { Checkbox, TextField, Button } from 'ui-neumorphism';
+import { useSnackbar } from 'notistack';
+
+const Item = ({ item: { id, name, quantity, created, complete } }) => {
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+  const [isItemCompleted, setIsItemCompleted] = useState(complete);
+  const [quantityOfItem, setQuantityOfItem] = useState(quantity);
+
+  const addOneItem = (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    let URL = process.env.REACT_APP_URL + 'items/' + id;
+    let object = JSON.stringify({
+      id: id,
+      name: name,
+      quantity: quantityOfItem,
+      created: created,
+      complese: isItemCompleted,
+    });
+    xhr.open('PUT', URL, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(object);
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        enqueueSnackbar('Amount of item increased.', {
+          variant: 'success',
+        });
+      }
+    };
+  };
+
+  const deleteOneItem = (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    let URL = process.env.REACT_APP_URL + 'items/' + id;
+    let object = JSON.stringify({
+      id: id,
+      name: name,
+      quantity: quantityOfItem,
+      created: created,
+      complese: isItemCompleted,
+    });
+    xhr.open('PUT', URL, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send(object);
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        enqueueSnackbar('Amount of item decreased.', {
+          variant: 'warning',
+        });
+      }
+    };
+  };
+
+  const deleteWholeItem = (e) => {
+    e.preventDefault();
+    let xhr = new XMLHttpRequest();
+    let URL = process.env.REACT_APP_URL + 'items/' + id;
+    xhr.open('DELETE', URL, true);
+    xhr.responseType = 'json';
+    xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhr.send();
+    xhr.onload = function () {
+      if (xhr.status == 200) {
+        enqueueSnackbar('Item is deleted.', {
+          variant: 'error',
+        });
+      }
+    };
+  };
+
+  const handleCheckBox = (e) => {
+    e.preventDefault();
+    setIsItemCompleted(!isItemCompleted);
+    enqueueSnackbar(isItemCompleted.toString(), {
+      variant: 'error',
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Checkbox
+        color="var(--primary)"
+        checked={isItemCompleted}
+        onClick={handleCheckBox}
+      />
+      <TextField label={name} className="my-3" readonly></TextField>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginRight: '25px',
+        }}
+      >
+        <Button color="var(--primary)" onClick={deleteOneItem}>
+          -
+        </Button>
+        <TextField
+          label={quantityOfItem}
+          width="60"
+          className="my-3"
+          type="number"
+        ></TextField>
+        <Button color="var(--primary)" onClick={addOneItem}>
+          +
+        </Button>
+      </div>
+      <Button color="red" onClick={deleteWholeItem}>
+        Delete
+      </Button>
+    </div>
+  );
+};
+
+export default Item;
